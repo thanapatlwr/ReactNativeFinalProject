9ba2c6d6b1a48b6f9a4b1edebfd45d80e4c8dd85
thanapatlwr <thanapatlwr@gmail.com>
2024-10-26 23:33:02 +0700

    updateFinal_6

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';  
+import HomeScreen from './HomeScreen';   
+import RegisterScreen from './RegisterScreen';
+import ForgetScreen from './ForgetScreen';
+import ProfileScreen from './ProfileScreen';
+import SetTimeScreen from './SetTimeScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Regis" component={RegisterScreen} />
+        <Stack.Screen name="Forget" component={ForgetScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="SetTime" component={SetTimeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
